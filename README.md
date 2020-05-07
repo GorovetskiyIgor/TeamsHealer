f0fce24e731ad1c055b53472e401324c290e26ae
Igor Gorovetskyi <unclav@gmail.com>
2020-05-07 11:54:18 +0300

    add script, update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TeamsHealer
+## Step 1
+Run `TeamsHealer.ps1` script in powershell
+
+![](images/step1.png)
+## Step 2
+Wait for cleanup and downloading latest Teams version
+
+## Step 3
+Teams will restart automatically. If auto-sign-in failed:
+
+![](images/step3.png)
+
+Click `signing out` link and follow login prosedure using Authenticator App.
+
+## ...
+
+## PROFIT 
---
+++ TeamsHealer.ps1
@@ -0,0 +1,26 @@
+﻿Stop-Process -Name 'Teams' -Force -ErrorAction SilentlyContinue
+
+#Clear teams tmp
+$teamsDir = $env:APPDATA + '\Microsoft\Teams'
+if (Test-Path $teamsDir){
+    Remove-Item "$teamsDir\Application Cache\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\blob_storage\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\Cache\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\databases\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\GPUCache\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\IndexedDb\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\Local Storage\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item "$teamsDir\tmp\*" -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+#Download latest Teams
+$Installdir = "c:\Apps\Install_Teams_TMP"   
+if(-Not (Test-Path $Installdir)){
+    New-Item -Path $Installdir -ItemType directory 
+}
+
+$source = "https://go.microsoft.com/fwlink/p/?linkid=869426&clcid=0x409&culture=en-us&country=us&lm=deeplink&lmsrc=groupchatmarketingpageweb&cmpid=directdownloadwin64"  
+$destination = "$Installdir\Teams_windows_x64.exe"  
+Invoke-WebRequest $source -OutFile $destination  
+
+Start-Process -FilePath "$Installdir\Teams_windows_x64.exe"
